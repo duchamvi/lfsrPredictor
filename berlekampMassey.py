023482016194bd4ed78ce49c73745f16e75106b9
duchamvi <vincent.duchamp91@gmail.com>
2019-02-06 10:49:43 +0000

    Removed unused code

--- berlekampMassey.py
+++ berlekampMassey.py
@@ -1,16 +1,3 @@
-def bitToInt(bits):
-    n = 0
-    for i in range (len(bits)):
-        n+= bits[i]*(2**i)
-    return n
-
-def intToBits(n, length):
-    bits = []
-    for i in range(length):
-        bits.append(n%2)
-        n = n//2
-    return bits
-
 def BerlekampMasseyAlgorithm(stream):
     N = len(stream)
 
